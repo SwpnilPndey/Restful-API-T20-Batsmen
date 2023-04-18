d23b20f1824e808006bd9ad532acbf14432e5f27
SwpnilPndey <Swpnil.Pndey@gmail.com>
2023-04-18 18:30:16 +0530

    changed CORs policy code

--- src/app.js
+++ src/app.js
@@ -15,10 +15,23 @@ app.use(express.json());
 app.use(router);
 
 
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     next();
+//   });
+
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-  });
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
 
 
 
