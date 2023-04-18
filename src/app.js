a323c84928ec100efe2c2974f943c440a6ceb738
SwpnilPndey <Swpnil.Pndey@gmail.com>
2023-04-18 18:44:38 +0530

    Installed Cors package

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express=require("express");
 const app=express();
 
+const cors=require("cors");
+app.use(cors());
+
 
 const port=process.env.PORT||3000;
 
@@ -20,18 +23,7 @@ app.use(router);
 //     next();
 //   });
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
-  );
-  next();
-});
+
 
 
 
